dcd70f811c27b5f9351b56e4994cc4e9dfba2fda
lavdrimmorina-ctrl <lavdrimmorina-ctrl@users.noreply.github.com>
2026-05-25 07:22:10 +0000

    deploy: c921fc51d14a9d433bcfa570c8e2cf38010f135c

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
 // Version is injected automatically at deploy time via GitHub Actions
 // Falls back to timestamp if not injected
-const VERSION = 'monarka-1779692977';
+const VERSION = 'monarka-1779693730';
 const ASSETS = ['./manifest.json'];
 
 self.addEventListener('install', e => {
