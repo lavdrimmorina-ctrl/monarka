a13dd1108e986252f9eea2fbfb4ee6405e9eb70e
lavdrimmorina-ctrl <lavdrimmorina-ctrl@users.noreply.github.com>
2026-05-25 06:43:45 +0000

    deploy: 6d06dcce2c1aa5d6f9839bbc82b92d15854cdd14

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
 // Version is injected automatically at deploy time via GitHub Actions
 // Falls back to timestamp if not injected
-const VERSION = 'monarka-1779691395';
+const VERSION = 'monarka-1779691425';
 const ASSETS = ['./manifest.json'];
 
 self.addEventListener('install', e => {
@@ -105,4 +105,8 @@ self.addEventListener('activate', e => {
       clients.forEach(client => client.postMessage({ type: 'SW_UPDATED', version: VERSION }));
     })
   );
+});
+
+self.addEventListener('message', e => {
+  if(e.data?.type === 'SKIP_WAITING') self.skipWaiting();
 });
